0beee57da5794b3d57f763e025b277544fd65ca8
Jason Campbell <43692594+jcampbell18@users.noreply.github.com>
2019-11-27 12:56:10 -0800

    Updated index.js
    merged index to redirect to Login page

--- inavig8-admin/src/index.js
+++ inavig8-admin/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
+//import ReactDOM from 'react-dom'
+import { render } from 'react-dom';
+import Router from './components/Router';
+//import App from './app'
+import './css/styles.css';
+import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+render(
+    <Router />, document.querySelector('#root')
+);
+
+serviceWorker.unregister();
