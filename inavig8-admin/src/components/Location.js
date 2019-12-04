3f3b94dd20bc16f8a590ce1da6452e10a363cdac
Jason C <jcampbell18@eagles.ewu.edu>
2019-12-03 19:13:08 -0800

    Updated Location.js

--- inavig8-admin/src/components/Location.js
+++ inavig8-admin/src/components/Location.js
@@ -6,12 +6,11 @@ class Location extends React.Component {
     render() {
 
         const { id, long_name } = this.props.details;
-        const href = '/location/' + id;
 
         return (
 
             <section className="location-list">
-                <h4>Location: <a href={href}>{long_name}</a></h4>
+                <h4>Location: <a href={`/location/${id}`}>{long_name}</a></h4>
                 <a href=""><img src="./img/example-map.jpg" alt={long_name} title={long_name} /></a>
             </section>
 
