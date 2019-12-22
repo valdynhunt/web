375aa0f82096295dbc5927a029ca320a9110f57d
Valdyn Hunt <valdyn@mac.com>
2019-12-22 11:44:38 -0800

    add components and modify renders

---
+++ inavig8-admin/src/components/Stairs.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image } from 'react-konva';
+import useImage from 'use-image';
+
+const MALE_SRC = '/img/icons/male.png'; 
+
+const Male = (props) => {
+    const [image] = useImage(MALE_SRC);
+    const handleMaleClick = (e) => {
+      console.log('male clicked');
+    }
+    return <Image 
+            x={props.x} 
+            y={props.y} 
+            scaleX={0.04} 
+            scaleY={0.04} 
+            // draggable              
+            // onDragStart={props.handleDragImageStart}
+            // onDragEnd={props.handleDragImageEnd}
+            onClick={handleMaleClick}
+          image={image} />;
+  };
+
+
+  export default Male;
---
+++ inavig8-admin/src/components/Utensils.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image } from 'react-konva';
+import useImage from 'use-image';
+
+const UTENSILS_SRC = '/img/icons/utensils.png'; 
+
+const Utensils = (props) => {
+    const [image] = useImage(UTENSILS_SRC);
+    const handleUtensilsClick = (e) => {
+      console.log('utensils clicked');
+    }
+    return <Image 
+            x={props.x} 
+            y={props.y} 
+            scaleX={0.04} 
+            scaleY={0.04} 
+            // draggable              
+            // onDragStart={props.handleDragImageStart}
+            // onDragEnd={props.handleDragImageEnd}
+            onClick={handleUtensilsClick}
+          image={image} />;
+  };
+
+
+  export default Utensils;
