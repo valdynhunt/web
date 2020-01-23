a236fb7076eb158ef808538e978c5c1d54ebd322
System Administrator <root@Najis-MacBook-Pro.local>
2020-01-22 16:45:38 -0800

    started export

---
+++ inavig8-admin/src/components/Export.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+class Export extends React.Component{
+    
+    render() { 
+        return (
+            <Button> Export </Button>
+        )
+    }
+}
+
+export default Export;
