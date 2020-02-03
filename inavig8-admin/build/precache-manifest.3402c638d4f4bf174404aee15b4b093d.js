81c90ec9840ef0e3d1fee2aa005a7b8c1472e2c9
jcampbell18 <jcampbell18@eagles.ewu.edu>
2020-02-03 11:09:13 -0800

    Initial build subtree commit

---
+++ inavig8-admin/build/precache-manifest.3402c638d4f4bf174404aee15b4b093d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162fff406770c6bc25c9a10db380fef4",
+    "url": "/web/inavig8-admin/index.html"
+  },
+  {
+    "revision": "e5eae729114df59fe41b",
+    "url": "/web/inavig8-admin/static/css/2.1c29a7ee.chunk.css"
+  },
+  {
+    "revision": "1fb622d7dd7bac4ee0ed",
+    "url": "/web/inavig8-admin/static/css/main.d952015c.chunk.css"
+  },
+  {
+    "revision": "e5eae729114df59fe41b",
+    "url": "/web/inavig8-admin/static/js/2.beb739ea.chunk.js"
+  },
+  {
+    "revision": "1fb622d7dd7bac4ee0ed",
+    "url": "/web/inavig8-admin/static/js/main.54343953.chunk.js"
+  },
+  {
+    "revision": "82124d9831f362b41114",
+    "url": "/web/inavig8-admin/static/js/runtime-main.690d1236.js"
+  },
+  {
+    "revision": "0698c4ef2c4d0092eb1f3686c17da734",
+    "url": "/web/inavig8-admin/static/media/background.0698c4ef.jpg"
+  }
+]);
